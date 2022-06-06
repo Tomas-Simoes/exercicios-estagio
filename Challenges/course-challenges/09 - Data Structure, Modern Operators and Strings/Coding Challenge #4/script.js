9e29d33409d0d8a0892fdacf3b9b04220b91bf77
Tomas-Simoes <tomas20simoes@gmail.com>
2022-06-06 12:27:35 +0100

    Updated Coding Challenge #4

--- Challenges/course-challenges/09 - Data Structure, Modern Operators and Strings/Coding Challenge #4/script.js
+++ Challenges/course-challenges/09 - Data Structure, Modern Operators and Strings/Coding Challenge #4/script.js
@@ -1,57 +1,44 @@
-/*
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
 
-Let's continue with our football betting app! This time, we have a map called
-'gameEvents' (see below) with a log of the events that happened during the
-game. The values are the events themselves, and the keys are the minutes in which
-each event happened (a football game has 90 minutes plus some extra time).
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
 
-Your tasks:
-1. Create an array 'events' of the different game events that happened (no
-duplicates)
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
 
-2. After the game has finished, is was found that the yellow card from minute 64
-was unfair. So remove this event from the game events log.
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
 
-3. Compute and log the following string to the console: "An event happened, on
-average, every 9 minutes" (keep in mind that a game has 90 minutes)
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
 
-4. Loop over 'gameEvents' and log each element to the console, marking
-whether it's in the first half or second half (after 45 min) of the game, like this:
-⚽
-[FIRST HALF] 17:
-GOAL
+Afterwards, test with your own test data!
+
 GOOD LUCK 😀
 */
+document.body.append(document.createElement("textArea"));
+document.body.append(document.createElement("button"));
 
-const gameEvents = new Map([
-  [17, "⚽ GOAL"],
-  [36, "🔁 Substitution"],
-  [47, "⚽ GOAL"],
-  [61, "🔁 Substitution"],
-  [64, "🔶 Yellow card"],
-  [69, "🔴 Red card"],
-  [70, "🔁 Substitution"],
-  [72, "🔁 Substitution"],
-  [76, "⚽ GOAL"],
-  [80, "⚽ GOAL"],
-  [92, "🔶 Yellow card"],
-]);
+const convertToCamel = () => {
+  const textToConvert = document
+    .querySelector("textarea")
+    .value.toLowerCase()
+    .split("\n");
 
-//? TASK 1
-const events = [...new Set(gameEvents.values())];
-console.log(events);
+  for (const word of textToConvert) {
+    const [first, second] = word.trim().split("_");
+    console.log(`${first}${second[0].toUpperCase() + second.slice(1)}`);
+  }
+};
 
-//? TASK 2
-gameEvents.delete(64);
-console.log(gameEvents);
-
-//? TASK 3
-console.log(
-  `An event happened, on average, every ${90 / gameEvents.size}  minutes`
-);
-
-//? TASK 4
-for (const [minute, event] of gameEvents.entries()) {
-  minute < 45 ? console.log("[FIRST HALF]") : console.log("[SECOND HALF]");
-  console.log(`: ${event}`);
-}
+document.querySelector("button").addEventListener("click", convertToCamel);
